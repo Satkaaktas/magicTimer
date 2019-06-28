3912facb00e53de33098da363edb3344b30d4c9d
Teodor Englund <teodor.englund@capgemini.com>
2019-06-28 14:58:50 +0200

    timer is working now

--- timerScript.js
+++ timerScript.js
@@ -1,3 +1,107 @@
+
+// variabels
+//shift + alt + f
+let minutes,
+    seconds,
+    inputTime,
+    timeLeft;
+
+inputTime = 50;
+endTime = Date.parse(new Date) + (inputTime * 60000);
+
+var minutesLeft = function () {
+    currentTime = Date.parse(new Date());
+    timeLeft = endTime - currentTime;
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    return minutes;
+}
+
+let startButton = document.getElementById("startTimer");
+var timeInput = document.getElementById("countdownTime");
+startButton.addEventListener("click", startCountdown);
+
+function getCountdownTime(){
+    console.log(timeInput.value);
+    setInterval(updateClock, 1000);
+}
+
+function startCountdown() {
+    getCountdownTime();
+    endTime = Date.parse(new Date) + (inputTime * 60000);
+}
+
+function calculateTimeLeft() {
+    currentTime = Date.parse(new Date());
+    timeLeft = endTime - currentTime;
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    return {m:minutes, s:seconds};
+}
+
+function updateClock() {
+
+    currentTime = Date.parse(new Date());
+    timeLeft = endTime - currentTime;
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("time").innerHTML = calculateTimeLeft();
+}
+
+/*
+let currentTime,
+    inputTime,
+    countdownTimeMilisec,
+    currentTimeMilisec,
+    endOfCountdownMilisec;
+
+
+inputTime = 1;
+currentTimeMilisec = getCurrentTimeInMilisec();
+countdownTimeMilisec = convertMinToMilisec(inputTime)
+endOfCountdownMilisec = countdownTimeMilisec + currentTimeMilisec
+console.log("countdowntime in milisec = " + countdownTimeMilisec);
+console.log("current time = " + currentTimeMilisec);
+console.log("Countdown end time = " + (endOfCountdownMilisec));
+console.log((5%7));
+//startCountdown();
+
+function getCurrentTimeInMilisec(){
+    return Date.parse(new Date());
+}
+
+function convertMinToMilisec(min){
+    // 1 minute = 60000 milliseconds
+    return min * 60000;
+}
+
+function convertMilisecToMin(milisec){
+    // 1 minute = 60000 milliseconds
+    return milisec / 60000;
+}
+
+function showTimeLeft(){
+    console.log(endOfCountdownMilisec - currentTimeMilisec);
+}
+
+function startCountdown(){
+    setInterval(timeLeft, 1000)
+}
+
+function timeLeft(){
+    if(endOfCountdownMilisec <= getCurrentTimeInMilisec()){
+        console.log("countdown completed")
+    }else{
+        console.log("min " + convertMilisecToMin)
+
+    }
+}
+
+*/
+
+//console.log(s);
+
+/*
 let timer = 5;
 let counter = 1;
 let timePreviousFrame;
@@ -26,7 +130,7 @@ function draw() {
         counter += 1;
         timer--;
     }
-    if (timer == 0) 
+    if (timer == 0)
     {
         text("GAME OVER", width / 2, height * 0.7);
     }
@@ -38,4 +142,4 @@ function deltaTime() {
     let _deltaTime = _time - timePreviousFrame;
     timePreviousFrame = _time;
     return (_deltaTime * 0.001);
-}
+}*/
